bfbf4d299000e3734b9bf9b3d8f4898e8efaf712
Wolfgang Schuster <wolfadex@gmail.com>
2023-02-02 22:12:42 -0600

    Minor test cleanup

--- tests/Test/Page/NewGameObserve.elm
+++ tests/Test/Page/NewGameObserve.elm
@@ -1,11 +1,15 @@
-module Test.Page.Home exposing (..)
+module Test.Page.NewGameObserve exposing (..)
 
-import Page.Home
+import Element
+import Fuzz
+import Page.NewGameObserve
+import ProgramTest
 import Random
 import Shared exposing (SharedModel)
 import Test exposing (Test)
 import Test.Element.Query as Query
 import Test.Html.Selector as Html
+import View
 
 
 testSharedModel : SharedModel
@@ -17,16 +21,45 @@ testSharedModel =
 
 suite : Test
 suite =
-    Test.describe "Renders the screen"
-        [ Test.test "renders the title and start button"
-            (\() ->
-                Query.has
-                    [ Html.text "Space Sim!"
-                    , Html.text "Participate"
-                    , Html.text "Observe"
-                    ]
-                    (Query.fromElement (.body (Page.Home.view testSharedModel Page.Home.baseModel)))
-            )
+    Test.describe "Renders the new game observation page"
+        [ Test.describe "view"
+            [ Test.fuzz2
+                Fuzz.int
+                Fuzz.int
+                "test  solar system count"
+                (\_ _ ->
+                    ProgramTest.createDocument
+                        { init = \_ -> Page.NewGameObserve.init
+                        , update = Page.NewGameObserve.update testSharedModel
+                        , view = View.viewToTestDocument (Element.layout []) (Page.NewGameObserve.view testSharedModel)
+                        }
+                        |> ProgramTest.start (Random.initialSeed 0)
+                        -- |> ProgramTest.clickLink "Observe"
+                        --     (Route.toString
+                        --         (Route.Playing
+                        --             { name = Data.Name.fromString ""
+                        --             , homePlanetName = ""
+                        --             , minSolarSystemsToGenerate = Page.NewGameObserve.baseModel.minSolarSystemsToGenerate
+                        --             , maxSolarSystemsToGenerate = Page.NewGameObserve.baseModel.maxSolarSystemsToGenerate
+                        --             , minPlanetsPerSolarSystemToGenerate = Page.NewGameObserve.baseModel.minPlanetsPerSolarSystemToGenerate
+                        --             , maxPlanetsPerSolarSystemToGenerate = Page.NewGameObserve.baseModel.maxPlanetsPerSolarSystemToGenerate
+                        --             , starCounts = Page.NewGameObserve.baseModel.starCounts
+                        --             , playType = Shared.Observation
+                        --             }
+                        --         )
+                        --     )
+                        |> ProgramTest.ensureView
+                            (Query.find [ Html.id "min-solar-system-count" ]
+                                >> Query.has
+                                    [ Html.text "40" ]
+                            )
+                        |> ProgramTest.expectView
+                            (Query.find [ Html.id "max-solar-system-count" ]
+                                >> Query.has
+                                    [ Html.text "80" ]
+                            )
+                )
+            ]
 
         -- , Test.describe "Participation view"
         --     [ Test.fuzz Fuzz.string
